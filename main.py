a39dd2826f3c97490933f8ba324f0f1d7d953c1d
hugoMontevideo <machadopereira.hugo@gmail.com>
2025-06-28 09:20:44 +0200

    implementing gps app

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from plyer import gps
+from kivy.utils import platform
+from kivy.clock import Clock
 
-class MyKivyApp(App):
+
+class GpsApp(App):
     def build(self):
-        return Label(text="Hello, Kivy!")
+        self.label = Label(text="En attente de la position GPS...")
+        if platform == "android":
+            try:
+                gps.configure(on_location=self.on_location, on_status=self.on_status)
+                gps.start(minTime=1000, minDistance=1)
+            except NotImplementedError:
+                self.label.text = "GPS non disponible sur cette plateforme"
+        else:
+            self.label.text = "GPS uniquement sur Android"
+        return self.label
 
-if __name__ == "__main__":
-    MyKivyApp().run()
+    def on_location(self, **kwargs):
+        lat = kwargs.get('lat', 'N/A')
+        lon = kwargs.get('lon', 'N/A')
+        self.label.text = f"Latitude: {lat}\nLongitude: {lon}"
+
+    def on_status(self, stype, status):
+        self.label.text = f"[{stype}] {status}"
+
+
+if __name__ == '__main__':
+    GpsApp().run()
 
 
